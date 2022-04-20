d1d7261408423b90459f2e2652d9f8671345adeb
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-04-20 11:37:52 +0700

    save

--- 1.12.js
+++ 1.12.js
@@ -1,10 +1,10 @@
 debugger;
 const draw = (x)=>{
     let ans = ''
-    let num = 0
+    let num = x-1
     for (let i = 0; i < x; i++) {
         for (let j = 0; j < x; j++) {
-            if(num != j){
+            if(num == j){
                 ans += "-"
             }
             else{
@@ -14,7 +14,7 @@ const draw = (x)=>{
             
         }
         ans += "\n"
-        num++
+        num--
        
         
     }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,25 @@
- if(j%2 == 0){
-                ans += "_"
+debugger;
+const draw = (x)=>{
+    let ans = ''
+    let num = 0
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < x; j++) {
+            if(num == j){
+                ans += "-"
             }
             else{
                 ans += "*"
             }
+            
+            
+        }
+        ans += "\n"
+        num++
+       
+        
+    }
+    console.log(ans)
+}
+draw(2)
+draw(3)
+draw(4)
